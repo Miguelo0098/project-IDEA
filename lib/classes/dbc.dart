abd07ba1b55834115a26856aac9ed5f6d9fa7c88
miguelo0098 <mugui098@gmail.com>
2019-11-13 21:53:33 +0100

    Completed DataBaseHelper

--- lib/classes/dbc.dart
+++ lib/classes/dbc.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+import 'idea.dart';
+
 class DataBaseHelper {
   Future<Database> get database async {
     return openDatabase(
@@ -15,6 +17,50 @@ class DataBaseHelper {
       },
 
       version: 1,
+    );
+  }
+
+  Future<void> insertIdea(Idea idea) async{
+    final Database db = await database;
+
+    await db.insert(
+      'ideas',
+      idea.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List <Idea>> ideas() async{
+    final Database db = await database;
+
+    final List<Map<String, dynamic>> maps = await db.query('ideas');
+
+    return List.generate(maps.length, (i){
+      return Idea(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        description: maps[i]['description'],
+      );
+    });
+  }
+
+  Future<void> updateIdea(Idea idea) async {
+    final Database db = await database;
+
+    await db.update('ideas', 
+      idea.toMap(),
+      where: "id = ?",
+      whereArgs: [idea.id],
+    );
+  }
+
+  Future<void> deleteIdea(int id) async{
+    final Database db = await database;
+
+    await db.delete(
+      'dogs',
+      where: "id = ?",
+      whereArgs: [id],
     );
   }
 }
